efd6f155cbadc49b0ed02f371c796b2964bbe1f6
MrHammadSandhu <mrhammadsandhu0@gmail.com>
2025-01-15 18:46:40 +0500

    changing file namess

--- index.js
+++ index.js
@@ -7,7 +7,22 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const userrouter = require("./Routes/user.route");
 connectdb();
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins or restrict to specific domains
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// Configure CORS
+const corsOptions = {
+  origin: ["*", "https://www.gulfhorizontele.com", "http://127.0.0.1:5500"], // Add local testing and production domains
+  methods: ["GET", "POST", "OPTIONS"], // Include OPTIONS for preflight requests
+};
+app.use(cors(corsOptions)); // Enable CORS
+app.options("*", cors(corsOptions)); // Handle preflight requests globally
 app.use(express.json());
 app.use(cookieParser());
 
